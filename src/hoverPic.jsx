dccb8f5f36e7a045fcc7cf78123572a29a240aa5
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-08-29 00:43:08 +0530

    optimised gsap code for react using useRef and useEffect

--- src/hoverPic.jsx
+++ src/hoverPic.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect,useRef} from "react";
 import Pic from "./pictures";
 import './App.css';
 import gsap from 'gsap';
@@ -20,75 +20,63 @@ function addPics(link){
 
 
 function HoverPics(){
-    return(
-        <div className="container-items">
-					<div className="container-img-wrap">
-						<div className="container-img"></div>
-					</div>
-                    {Pic.map(addPics)}
-				</div>
-        
-    )
-}
 
-export default HoverPics;
+	const venueImage=useRef();
+const venueImageWrap=useRef();
 
 
+useEffect(()=>{
+	const allcontainer = gsap.utils.toArray(".container-item");
+	// const venueImageWrap = document.querySelector(".container-img-wrap");
+	// const venueImage = document.querySelector(".container-img");
 
-document.addEventListener('readystatechange', event => { 
-
-        const allcontainer = gsap.utils.toArray(".container-item");
-		const venueImageWrap = document.querySelector(".container-img-wrap");
-		const venueImage = document.querySelector(".container-img");
-
-		function initcontainer() {
-			allcontainer.forEach((link) => {
-				link.addEventListener("mouseenter", venueHover);
-				link.addEventListener("mouseleave", venueHover);
-				link.addEventListener("mousemove", moveVenueImage);
-			});
-		}
-
-		function moveVenueImage(e) {
-			let xpos = e.clientX;
-			let ypos = e.clientY;
-			const tl = gsap.timeline();
-			tl.to(venueImageWrap, {
-				x: xpos,
-				y: ypos,
-			});
-		}
-
-		function venueHover(e) {
-			if (e.type === "mouseenter") {
-				const targetImage = e.target.dataset.img;
-
-				const tl = gsap.timeline();
-				tl.set(venueImage, {
-					backgroundImage: `url(${targetImage})`,
-				}).to(venueImageWrap, {
-					duration: 0.5,
-					autoAlpha: 1,
-				});
-			} else if (e.type === "mouseleave") {
-				const tl = gsap.timeline();
-				tl.to(venueImageWrap, {
-					duration: 0.5,
-					autoAlpha: 0,
-				});
-			}
-		}
-
-		function init() {
-			initcontainer();
-		}
-
-		window.addEventListener("load", function () {
-			init();
+	function initcontainer() {
+		allcontainer.forEach((link) => {
+			link.addEventListener("mouseenter", venueHover);
+			link.addEventListener("mouseleave", venueHover);
+			link.addEventListener("mousemove", moveVenueImage);
 		});
-    });
+	}
 
+	function moveVenueImage(e) {
+		let xpos = e.clientX;
+		let ypos = e.clientY;
 		const tl = gsap.timeline();
+		tl.to(venueImageWrap.current, {
+			x: xpos,
+			y: ypos,
+		});
+	}
+
+	function venueHover(e) {
+		if (e.type === "mouseenter") {
+			const targetImage = e.target.dataset.img;
+
+			const tl = gsap.timeline();
+			tl.set(venueImage.current, {
+				backgroundImage: `url(${targetImage})`,
+			}).to(venueImageWrap.current, {
+				duration: 0.5,
+				autoAlpha: 1,
+			});
+		} else if (e.type === "mouseleave") {
+			const tl = gsap.timeline();
+			tl.to(venueImageWrap.current, {
+				duration: 0.5,
+				autoAlpha: 0,
+			});
+		}
+	}
+
+	function init() {
+		initcontainer();
+	}
+
+	window.addEventListener("load", function () {
+		init();
+	});
+
+	const tl = gsap.timeline();
 
 		
         
@@ -117,5 +105,139 @@ document.addEventListener('readystatechange', event => {
 			},
 			
 		);
+},[])
+
+    return(
+        <div  className="container-items">
+					<div ref={venueImageWrap} className="container-img-wrap">
+						<div ref={venueImage} className="container-img"></div>
+					</div>
+                    {Pic.map(addPics)}
+				</div>
+        
+    )
+}
+
+// const tl = gsap.timeline();
+
+		
+        
+// 		tl.from(
+// 			".site-logo",
+			
+// 			{duration:1.6,
+// 				opacity: 0,
+// 				y: 40,
+// 				ease: "expo.inOut",
+// 			},
+// 			"-=1.6"
+// 		);
+
+		
+
+// 		tl.from(
+// 			".header > div",
+			
+// 			{   duration:1,
+// 				opacity: 0,
+// 				y: 60,
+// 				ease: "power2.Out",
+// 				delay: -1.4,
+//                 stagger:0.2
+// 			},
+			
+// 		);
+
+// const venueImage=useRef();
+// const venueImageWrap=useRef();
+
+
+// useEffect(()=>{
+// 	const allcontainer = gsap.utils.toArray(".container-item");
+// 	// const venueImageWrap = document.querySelector(".container-img-wrap");
+// 	// const venueImage = document.querySelector(".container-img");
+
+// 	function initcontainer() {
+// 		allcontainer.forEach((link) => {
+// 			link.addEventListener("mouseenter", venueHover);
+// 			link.addEventListener("mouseleave", venueHover);
+// 			link.addEventListener("mousemove", moveVenueImage);
+// 		});
+// 	}
+
+// 	function moveVenueImage(e) {
+// 		let xpos = e.clientX;
+// 		let ypos = e.clientY;
+// 		const tl = gsap.timeline();
+// 		tl.to(venueImageWrap, {
+// 			x: xpos,
+// 			y: ypos,
+// 		});
+// 	}
+
+// 	function venueHover(e) {
+// 		if (e.type === "mouseenter") {
+// 			const targetImage = e.target.dataset.img;
+
+// 			const tl = gsap.timeline();
+// 			tl.set(venueImage, {
+// 				backgroundImage: `url(${targetImage})`,
+// 			}).to(venueImageWrap, {
+// 				duration: 0.5,
+// 				autoAlpha: 1,
+// 			});
+// 		} else if (e.type === "mouseleave") {
+// 			const tl = gsap.timeline();
+// 			tl.to(venueImageWrap, {
+// 				duration: 0.5,
+// 				autoAlpha: 0,
+// 			});
+// 		}
+// 	}
+
+// 	function init() {
+// 		initcontainer();
+// 	}
+
+// 	window.addEventListener("load", function () {
+// 		init();
+// 	});
+
+// 	const tl = gsap.timeline();
+
+		
+        
+// 		tl.from(
+// 			".site-logo",
+			
+// 			{duration:1.6,
+// 				opacity: 0,
+// 				y: 40,
+// 				ease: "expo.inOut",
+// 			},
+// 			"-=1.6"
+// 		);
+
+		
+
+// 		tl.from(
+// 			".header > div",
+			
+// 			{   duration:1,
+// 				opacity: 0,
+// 				y: 60,
+// 				ease: "power2.Out",
+// 				delay: -1.4,
+//                 stagger:0.2
+// 			},
+			
+// 		);
+// },[])
+// document.addEventListener('readystatechange', event => { 
+
+
+//     });
+
+export default HoverPics;	
         
         
